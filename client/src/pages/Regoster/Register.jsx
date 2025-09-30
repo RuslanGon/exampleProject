ac2a0bdfea6069ab7f62bd1706350cb04dcf5d5b
RuslanGon <ruslan441006@gmail.com>
2025-09-30 15:37:38 +0300

    wer

--- client/src/pages/Regoster/Register.jsx
+++ client/src/pages/Regoster/Register.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import css from './Register.module.css'
+import google from '../../../public/google.svg'
+import facebook from '../../../public/face.svg'
+import link from '../../../public/link.svg'
 
 const Register = () => {
   return (
     <div className={css.container}>
-        <form action="">
+        <form className={css.form} action="">
             <h1 className={css.title}>Welcome to PlasmAI</h1>
             <p className={css.text}>Sign in to access your personalized blood test <br /> analysis. Your data is secure and private</p>
+            <button><img src={google}  alt="" />Sign in with Google</button>
+            <button><img src={facebook} alt="" />Sign in with Facebook</button>
+            <button><img src={link} alt="" />Sign in with Linkedin</button>
         </form>
 
     </div>
