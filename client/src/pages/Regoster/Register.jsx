6b8e652590cfd5130c5645fb2ee711151d21e968
RuslanGon <ruslan441006@gmail.com>
2025-09-30 16:12:18 +0300

    wer

--- client/src/pages/Regoster/Register.jsx
+++ client/src/pages/Regoster/Register.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import css from './Register.module.css'
-import google from '../../../public/google.svg'
-import facebook from '../../../public/face.svg'
-import link from '../../../public/link.svg'
+import React from "react";
+import css from "./Register.module.css";
+import google from "../../../public/google.svg";
+import facebook from "../../../public/face.svg";
+import link from "../../../public/link.svg";
 
 const Register = () => {
   return (
     <div className={css.container}>
-        <form className={css.form} action="">
-            <h1 className={css.title}>Welcome to PlasmAI</h1>
-            <p className={css.text}>Sign in to access your personalized blood test <br /> analysis. Your data is secure and private</p>
-            <button className={css.btn}><img className={css.logo} src={google}  alt="" />Sign in with Google</button>
-            <button className={css.btn}><img className={css.logo} src={facebook} alt="" />Sign in with Facebook</button>
-            <button className={css.btn}><img className={css.logo} src={link} alt="" />Sign in with Linkedin</button>
-            <div className={css.div_or}>
-                <hr className={css.hr} />
-                <span className={css.span}>Or</span>
-                <hr className={css.hr} />
-            </div>
-            <label className={css.lable} htmlFor="email">Email
-            <input className={css.input} id="email" type="text" name="email" />
-            </label>
-           
-        </form>
-
+      <form className={css.form} action="">
+        <h1 className={css.title}>Welcome to PlasmAI</h1>
+        <p className={css.text}>
+          Sign in to access your personalized blood test <br /> analysis. Your
+          data is secure and private
+        </p>
+        <button className={css.btn}>
+          <img className={css.logo} src={google} alt="" />
+          Sign in with Google
+        </button>
+        <button className={css.btn}>
+          <img className={css.logo} src={facebook} alt="" />
+          Sign in with Facebook
+        </button>
+        <button className={css.btn}>
+          <img className={css.logo} src={link} alt="" />
+          Sign in with Linkedin
+        </button>
+        <div className={css.div_or}>
+          <hr className={css.hr} />
+          <span className={css.span}>Or</span>
+          <hr className={css.hr} />
+        </div>
+        <label className={css.field}>
+          <span className={css.labelText}>Email</span>
+          <input className={css.input} id="email" type="text" name="email" placeholder="john.brown@gmail.com" />
+        </label>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
