bb13b441fc62a5e89e4559ec3fee5d602a2593c5
RuslanGon <ruslan441006@gmail.com>
2025-10-01 16:18:49 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,18 @@ import { useState } from 'react'
 
 import './App.css'
 import Register from './pages/Regoster/Register.jsx'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-  <Register />
-    </>
+  <Routes>
+     <Route path="/" element={<HomePage />} />
+     <Route path="/register" element={<Register />} />
+
+  </Routes>
   )
 }
 
